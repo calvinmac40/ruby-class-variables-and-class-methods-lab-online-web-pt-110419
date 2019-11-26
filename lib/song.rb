7163940b1b1b9215e54f008311a8e26bed8a0558
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-26 00:15:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
   end
   
   def self.artist_count
-    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+    #@@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
     binding.pry
   end
   
